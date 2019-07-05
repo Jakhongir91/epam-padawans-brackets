5ca162b0c78c3b996ff4516924dd47aec20f2b26
Jakhongir Nishanbaev <jakhongir.nishanbaev@gmail.com>
2019-07-05 15:59:45 +0500

    Algorithm implemented, not deleting last brackets sub string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strings = [];
+
+    for (let i=0; i<bracketsConfig.length; i++) {
+        strings.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+
+    while (subStringExists(str, strings) != -1) {
+        str = subStringExists(str, strings);
+        console.log(str);
+    }
+
+    return str;
+}
+
+function subStringExists(str, strings) {
+    let i=0;
+    for (i=0; i<strings.length; i++) {
+        let index = str.indexOf(strings[i]);
+
+        if (index > 0) {
+            str = str.replace(strings[i], "");
+
+            return str;
+        }
+    }
+
+    console.log(i);
+    console.log(strings[i]);
+    console.log(str);
+    console.log(-1);
+
+    return -1;
 }
